33b2b8ae91d5625f5bb7aa2eb28ce201e711800d
sandeep <sandeepbhaitia9040@gmail.com>
2020-09-07 18:24:14 +0530

    almost complete

---
+++ src/components/Pages/Search/loader.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './loader.css'
+
+
+
+
+   const Loader = (props) =>{
+       if(props.loader === "true"){
+        return(
+            <div>
+                 <div className="spinner">
+                <div className="bounce1"></div>
+                <div className="bounce2"></div>
+                <div className="bounce3"></div>
+            </div>
+            </div>)
+        }
+        else{
+            return(
+            <div></div>
+            )
+        }
+}
+export default Loader;
